12414b5e6c1a7b297f9ec2388fdb74a6f72d83e4
khazelton77 <keri.hazelton@gmail.com>
2016-08-16 16:27:18 -0400

    Stripe bug fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.secrets.publishable_key
-  :secret_key      => Rails.application.secrets.secret_key
-}
-
+  :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'], 
+  :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+  }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
